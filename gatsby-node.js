d69813e56c92a76f167c881459c66dbf351dee0f
Tom VanAntwerp <tom@tomvanantwerp.com>
2019-01-31 06:40:36 -0500

    Comment out page creation from markdown temporarily

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,38 +4,38 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-const path = require('path');
+// const path = require('path');
 
-exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions;
-  return new Promise((resolve, reject) => {
-    graphql(`
-      {
-        allMarkdownRemark {
-          edges {
-            node {
-              frontmatter {
-                path
-              }
-            }
-          }
-        }
-      }
-    `).then(res => {
-      if (res.errors) {
-        return reject(res.errors);
-      }
+// exports.createPages = ({ graphql, actions }) => {
+//   const { createPage } = actions;
+//   return new Promise((resolve, reject) => {
+//     graphql(`
+//       {
+//         allMarkdownRemark {
+//           edges {
+//             node {
+//               frontmatter {
+//                 path
+//               }
+//             }
+//           }
+//         }
+//       }
+//     `).then(res => {
+//       if (res.errors) {
+//         return reject(res.errors);
+//       }
 
-      res.data.allMarkdownRemark.edges.forEach(({ node }) => {
-        createPage({
-          path: `/${node.frontmatter.path}`,
-          component: path.resolve('./src/components/layout.js'),
-          context: {
-            path: node.frontmatter.path,
-          },
-        });
-      });
-      resolve();
-    });
-  });
-};
+//       res.data.allMarkdownRemark.edges.forEach(({ node }) => {
+//         createPage({
+//           path: `/${node.frontmatter.path}`,
+//           component: path.resolve('./src/components/layout.js'),
+//           context: {
+//             path: node.frontmatter.path,
+//           },
+//         });
+//       });
+//       resolve();
+//     });
+//   });
+// };
