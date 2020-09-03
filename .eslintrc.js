765eac4830af2fc483d2c9ec01673ab911e41827
Tom VanAntwerp <tom@tomvanantwerp.com>
2020-09-03 15:46:53 -0400

    Blow away Gatsby and install Next with TS and ESlint and Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,46 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    node: true
+  root: true, // Make sure eslint picks up the config at the root of the directory
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020, // Use the latest ecmascript standard
+    sourceType: 'module', // Allows using import/export statements
+    ecmaFeatures: {
+      jsx: true, // Enable JSX since we're using React
+    },
   },
+  settings: {
+    react: {
+      version: 'detect', // Automatically detect the react version
+    },
+  },
+  env: {
+    browser: true, // Enables browser globals like window and document
+    amd: true, // Enables require() and define() as global variables as per the amd spec.
+    node: true, // Enables Node.js global variables and Node.js scoping.
+  },
+  plugins: ['simple-import-sort'],
   extends: [
     'eslint:recommended',
-    'prettier',
-    'prettier/react'
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended', // Make this the last element so prettier config overrides other formatting rules
   ],
-  parserOptions: {
-    ecmaFeatures: {
-      experimentalObjectRestSpread: true,
-      jsx: true
-    },
-    sourceType: 'module'
-  },
-  plugins: ['react', 'prettier'],
   rules: {
-    'linebreak-style': ['error', 'unix'],
-    'react/jsx-uses-vars': 2,
-    'react/jsx-uses-react': 2,
-    'prettier/prettier': [
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }], // Use our .prettierrc file as source
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'simple-import-sort/sort': 'error',
+    'jsx-a11y/anchor-is-valid': [
       'error',
       {
-        trailingComma: 'es5',
-        singleQuote: true,
-        printWidth: 120
-      }
-    ]
-  }
+        components: ['Link'],
+        specialLink: ['hrefLeft', 'hrefRight'],
+        aspects: ['invalidHref', 'preferButton'],
+      },
+    ],
+  },
 };
