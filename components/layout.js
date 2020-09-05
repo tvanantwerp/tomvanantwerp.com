07d377f3d888840707f4270788e9b496704f6a65
Tom VanAntwerp <tom@tomvanantwerp.com>
2020-09-05 10:53:27 -0400

    Start creating global styles

--- components/layout.js
+++ components/layout.js
@@ -1,11 +1,41 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
 
 export const siteTitle = 'Tom VanAntwerp';
+
+const GlobalStyle = createGlobalStyle`
+:root {
+  --light-bg: #fff;
+  --light-color: #333;
+  --dark-bg: #333;
+  --dark-color: #fff;
+}
+
+html, body {
+  margin: 0;
+  padding: 0;
+
+  @media (prefers-color-scheme: light) {
+    background-color: var(--ligt-bg);
+    color: var(--light-color);
+  }
+
+  @media (prefers-color-scheme: dark) {
+    background-color: var(--dark-bg);
+    color: var(--dark-color);
+  }
+}
+
+* {
+  box-sizing: border-box;
+}
+`;
 
 export default function Layout({ children }) {
   return (
     <div>
+      <GlobalStyle />
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <meta
