385498175f0b5fc39327db9652e5f6ddf263abbf
Tom VanAntwerp <tom@tomvanantwerp.com>
2020-11-16 10:18:35 -0500

    Add some social links to footer

--- components/footer.js
+++ components/footer.js
@@ -6,12 +6,17 @@ import { siteTitle } from '@components/head';
 const StyledFooter = styled.footer`
   font-size: 0.8rem;
   text-align: center;
+
+  a {
+    color: inherit;
+  }
 `;
 
 export default function Footer() {
   return (
     <StyledFooter>
       <p>{`© ${siteTitle} ${new Date().getFullYear()}`}</p>
+      <p>Find me at <a href="https://twitter.com/tvanantwerp" target="_blank" rel="noopener noreferrer">Twitter</a> and <a href="https://github.com/tvanantwerp/" target="_blank" rel="noopener noreferrer">GitHub</a>.</p>
     </StyledFooter>
   );
 }
