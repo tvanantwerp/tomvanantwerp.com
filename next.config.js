8ac6e51be4811cc582e9dcbeb5c27c904cffb326
Tom VanAntwerp <tom@tomvanantwerp.com>
2021-01-14 16:10:10 -0500

    Handle markdown and mdx

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
-module.exports = {
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/
+});
+
+module.exports = withMDX({
   poweredByHeader: false,
   reactStrictMode: true,
-}
+  pageExtensions: ["js", "jsx", "md", "mdx"]
+});
