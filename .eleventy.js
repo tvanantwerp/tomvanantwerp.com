5647fd4f0cc8714ef16c5fb3c7d65a4c9533d684
Tom VanAntwerp <tom@tomvanantwerp.com>
2021-01-25 11:50:45 -0500

    Move more styles into scss

--- .eleventy.js
+++ .eleventy.js
@@ -5,11 +5,13 @@ const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginNavigation = require("@11ty/eleventy-navigation");
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
+const pluginSass = require("eleventy-plugin-sass");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(pluginSyntaxHighlight);
   eleventyConfig.addPlugin(pluginNavigation);
+  eleventyConfig.addPlugin(pluginSass);
 
   eleventyConfig.setDataDeepMerge(true);
 
@@ -68,6 +70,7 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("favicon");
 
   /* Markdown Overrides */
   let markdownLibrary = markdownIt({
