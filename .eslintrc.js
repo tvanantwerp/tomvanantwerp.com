a20e15397b53b676592c511dfb909f6172326cd5
Tom VanAntwerp <tom@tomvanantwerp.com>
2021-04-02 08:57:07 -0400

    Replace meta image default

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,24 @@
 module.exports = {
-  root: true, // Make sure eslint picks up the config at the root of the directory
-  parserOptions: {
-    ecmaVersion: 2020, // Use the latest ecmascript standard
-    sourceType: 'module', // Allows using import/export statements
-    ecmaFeatures: {
-      jsx: true, // Enable JSX since we're using React
-    },
-  },
-  env: {
-    browser: true, // Enables browser globals like window and document
-    amd: true, // Enables require() and define() as global variables as per the amd spec.
-    node: true, // Enables Node.js global variables and Node.js scoping.
-  },
-  plugins: ['simple-import-sort'],
-  extends: [
-    'eslint:recommended',
-    'plugin:prettier/recommended', // Make this the last element so prettier config overrides other formatting rules
-  ],
-  rules: {
-    'prettier/prettier': ['warn', {}, { usePrettierrc: true }], // Use our .prettierrc file as source
-    'simple-import-sort/sort': 'error',
-  },
+	root: true, // Make sure eslint picks up the config at the root of the directory
+	parserOptions: {
+		ecmaVersion: 2020, // Use the latest ecmascript standard
+		sourceType: 'module', // Allows using import/export statements
+		ecmaFeatures: {
+			jsx: true, // Enable JSX since we're using React
+		},
+	},
+	env: {
+		browser: true, // Enables browser globals like window and document
+		amd: true, // Enables require() and define() as global variables as per the amd spec.
+		node: true, // Enables Node.js global variables and Node.js scoping.
+	},
+	plugins: ['simple-import-sort'],
+	extends: [
+		'eslint:recommended',
+		'plugin:prettier/recommended', // Make this the last element so prettier config overrides other formatting rules
+	],
+	rules: {
+		'prettier/prettier': ['warn', {}, { usePrettierrc: true }], // Use our .prettierrc file as source
+		'simple-import-sort/sort': 'error',
+	},
 };
