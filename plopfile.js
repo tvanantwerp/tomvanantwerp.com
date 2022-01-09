d3168ed38f88452df4e8529235cdf0f18c19711a
Tom VanAntwerp <tvanantwerp@users.noreply.github.com>
2022-01-09 11:39:07 -0500

    Use plop to scaffold leetcode questions (#40)

---
+++ plopfile.js
@@ -0,0 +1,33 @@
+const _ = require('lodash');
+
+module.exports = function (plop) {
+	plop.setHelper('leadingZero', num => _.padStart(num.toString(), 3, '0'));
+	plop.setHelper('kebabCase', _.kebabCase);
+	plop.setGenerator('leetcode', {
+		description: 'Scaffold a leetcode article.',
+		prompts: [
+			{
+				type: 'input',
+				name: 'number',
+				message: 'What number?',
+			},
+			{
+				type: 'input',
+				name: 'name',
+				message: 'What is the name?',
+			},
+			{
+				type: 'input',
+				name: 'link',
+				message: 'What is the URL of the problem?',
+			},
+		],
+		actions: [
+			{
+				type: 'add',
+				path: `src/coding-questions/leetcode-{{leadingZero number}}-{{kebabCase name}}.md`,
+				templateFile: 'plop-templates/leetcode.hbs',
+			},
+		],
+	});
+};
