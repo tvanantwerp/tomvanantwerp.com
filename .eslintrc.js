0b880aa0a25c29b4e6083446f836d352cefb8339
Tom VanAntwerp <tom@tomvanantwerp.com>
2022-05-03 21:35:39 -0400

    Update eslint for typescript and react in astro

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-	root: true, // Make sure eslint picks up the config at the root of the directory
+	parser: '@typescript-eslint/parser',
 	parserOptions: {
 		ecmaVersion: 2020, // Use the latest ecmascript standard
 		sourceType: 'module', // Allows using import/export statements
@@ -12,13 +12,13 @@ module.exports = {
 		amd: true, // Enables require() and define() as global variables as per the amd spec.
 		node: true, // Enables Node.js global variables and Node.js scoping.
 	},
-	plugins: ['simple-import-sort'],
 	extends: [
-		'eslint:recommended',
-		'plugin:prettier/recommended', // Make this the last element so prettier config overrides other formatting rules
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react/recommended',
+		'plugin:react-hooks/recommended',
+		'prettier',
 	],
 	rules: {
-		'prettier/prettier': ['warn', {}, { usePrettierrc: true }], // Use our .prettierrc file as source
-		'simple-import-sort/sort': 'error',
+		'react/react-in-jsx-scope': 'off',
 	},
 };
