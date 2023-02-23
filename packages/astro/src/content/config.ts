78082dd4051c1a48e73b6ab9a7df5d954e26f529
Tom VanAntwerp <tom@tomvanantwerp.com>
2023-02-23 09:25:34 -0500

    Define essays schema

--- packages/astro/src/content/config.ts
+++ packages/astro/src/content/config.ts
@@ -7,7 +7,26 @@ const dataStructuresAndAlgorithms = defineCollection({
 		link: z.string().optional(),
 	}),
 });
-// 3. Export a single `collections` object to register your collection(s)
+const essays = defineCollection({
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		date: z.string().transform(str => new Date(str)),
+		updated: z
+			.string()
+			.transform(str => new Date(str))
+			.optional(),
+		emoji: z.string().optional(),
+		tags: z.array(z.string()).optional(),
+		layout: z.string().optional(),
+		image: z
+			.string()
+			.regex(/^img\/.*\.(?:jpg|jpeg|png|webp|gif|avif)/)
+			.optional(),
+	}),
+});
+
 export const collections = {
 	'ds-and-a': dataStructuresAndAlgorithms,
+	essays,
 };
