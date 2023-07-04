dc3443e7a6006a0c0bad612feb13ce458f831f3c
Tom VanAntwerp <tom@tomvanantwerp.com>
2023-07-04 12:05:38 -0400

    Set up images Astro dependency

--- packages/astro/astro.config.mjs
+++ packages/astro/astro.config.mjs
@@ -3,10 +3,18 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import turbolinks from '@astrojs/turbolinks';
 import sitemap from '@astrojs/sitemap';
+import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [react(), turbolinks(), sitemap()],
+	integrations: [
+		react(),
+		turbolinks(),
+		sitemap(),
+		image({
+			serviceEntryPoint: '@astrojs/image/sharp',
+		}),
+	],
 	markdown: {
 		syntaxHighlight: 'prism',
 	},
