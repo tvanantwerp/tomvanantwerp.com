48945271e2fdd21b934458a9ccb647494342e50a
Tom VanAntwerp <tom@tomvanantwerp.com>
2023-07-05 13:50:02 -0400

    Fix some weird prettier and eslint conflict

--- packages/astro/.eslintrc.js
+++ packages/astro/.eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
 	},
 	rules: {
 		'react/react-in-jsx-scope': 'off',
+		'react/jsx-closing-bracket-location': [1, 'line-aligned'],
 	},
 	settings: {
 		'mdx/code-blocks': true,
